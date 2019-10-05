e42f2591e01e46f1a261d42ba593fe85320d48c0
Marat Hakimov <Kshisa1973@gmail.com>
2019-10-05 12:07:11 +0300

    card

--- models/card.js
+++ models/card.js
@@ -2,7 +2,7 @@ const yaml = require('js-yaml');
 const fs   = require('fs');
 
 module.exports = foto = (req) => {
-  var first = yaml.safeLoad(fs.readFileSync('../../Base/00', 'utf8'));
+  let ft = yaml.safeLoad(fs.readFileSync('base/00', 'utf8'));
   var numb = Number(req.query.numb)
-  return [[first[numb][1][1]], [first[numb][1][0]]]
+  return [[ft[numb][1][1]], [ft[numb][1][0]]]
 }
